a2c3e2b72539c5a2c5070cd66dd56c941c7ee0ab
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-23 15:44:23 +0100

    YL -properties

--- App.js
+++ App.js
@@ -6,11 +6,26 @@ class App extends React.Component {
         //let is scoped to the nearest enclosing block
         //var is scoped to the nearest function block
         let txt = this.props.txt;
-        return <h1> {txt} </h1>;
+        return (
+            <div>
+                <h1> {txt} </h1>
+                <h2>{this.props.cat}</h2>
+            </div>
+        );
     }
 }
 
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: 'this is my first component'
+}
+
 ReactDOM.render(
-    <App txt="this is the props text" />,
+    <App cat={5} txt="this is the props text" />,
+    // <App cat={5} />, then will show the default props.txt
     document.getElementById('app')
 );
