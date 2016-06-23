9e3ea713c9b56bec6e64c577e603cea649813b5c
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-23 15:37:39 +0100

    YL - let vs var

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     render() {
-        return <h1> {this.props.txt} </h1>;
+        //let is scoped to the nearest enclosing block
+        //var is scoped to the nearest function block
+        let txt = this.props.txt;
+        return <h1> {txt} </h1>;
     }
 }
 
