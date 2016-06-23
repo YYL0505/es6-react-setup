ef0a143dafa5eda91fe0e51bd5fbdb4fa41795fe
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-23 15:54:53 +0100

    YL - use const component in component

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ class App extends React.Component {
         super();
         this.state = {
             txt: 'this is the state txt.'
-        }
+        };
+        this.update = this.update.bind(this);
     }
 
     update(e) {
@@ -18,15 +19,22 @@ class App extends React.Component {
     render() {
         //let is scoped to the nearest enclosing block
         //var is scoped to the nearest function block
-        let txt = this.props.txt;
         return (
             <div>
-                <input type="text" onChange={this.update.bind(this)}/>
-
-                <h1> {this.state.txt} </h1>
+                <Widget txt={this.state.txt} update={this.update} />
             </div>
         );
     }
+}
+
+const Widget = (props) => {
+    return (
+        <div>
+            <input type="text" onChange={props.update}/>
+
+            <h1> {props.txt} </h1>
+        </div>
+    );
 }
 
 export default App
