101a7458cd5fff900aac9961ca8028fa78c0ede6
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-23 16:20:17 +0100

    YL - use refs to access component.

--- App.js
+++ App.js
@@ -5,14 +5,22 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            txt: 'this is the state txt.'
+            txt: 'this is the state txt.',
+            red: 0,
+            green: 0,
+            blue: 0,
         };
         this.update = this.update.bind(this);
     }
 
     update(e) {
+        console.log(this.refs.red);
+        console.log(this.refs.red.refs.inp);
         this.setState({
-            txt: e.target.value
+            txt: e.target.value,
+            red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+            green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+            blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value,
         });
     }
 
@@ -21,20 +29,33 @@ class App extends React.Component {
         //var is scoped to the nearest function block
         return (
             <div>
-                <Widget txt={this.state.txt} update={this.update} />
+                <Slider ref="red" update={this.update} />
+                {this.state.red}
+                <br/>
+                <Slider ref="green" update={this.update} />
+                {this.state.green}
+                <br/>
+                <Slider ref="blue" update={this.update} />
+                {this.state.blue}
+                <br/>
             </div>
         );
     }
 }
 
-const Widget = (props) => {
-    return (
-        <div>
-            <input type="text" onChange={props.update}/>
+class Slider  extends React.Component{
+    render() {
+        return (
+            <div>
+                <input ref="inp"
+                       type="range"
+                       min="0"
+                       max="255"
+                       onChange={this.props.update}/>
+            </div>
+        );
+    }
 
-            <h1> {props.txt} </h1>
-        </div>
-    );
 }
 
 export default App
