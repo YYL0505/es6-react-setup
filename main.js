e266869ccc564e0dba7c211cf56c6ce98cdde4c3
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-23 16:43:24 +0100

    YL - access child properties.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import AppProps from './AppProps'
 
 ReactDOM.render(
-    <App />,
+    <AppProps />,
     document.getElementById('app')
 );
