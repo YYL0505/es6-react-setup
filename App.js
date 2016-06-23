8593b61181139bac664ecfef2dd45ed05ad7c57c
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-23 14:57:55 +0100

    YL - create component  with static method

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-class App extends React.Component {
-	render() {
-		return React.createElement('div', null, 'hello');
-	}
-}
+// class App extends React.Component {
+// 	render() {
+// 		return React.createElement('div', null, 'hello');
+// 	}
+// }
+
+const App = () => <div> hello </div>
 
 export default App
