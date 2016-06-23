52791d92a91650f4d874329b9d944218ec72f298
Yingli Yuan <ylyuan@thoughtworks.com>
2016-06-23 15:49:55 +0100

    YL -state

--- App.js
+++ App.js
@@ -2,30 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            txt: 'this is the state txt.'
+        }
+    }
+
+    update(e) {
+        this.setState({
+            txt: e.target.value
+        });
+    }
+
     render() {
         //let is scoped to the nearest enclosing block
         //var is scoped to the nearest function block
         let txt = this.props.txt;
         return (
             <div>
-                <h1> {txt} </h1>
-                <h2>{this.props.cat}</h2>
+                <input type="text" onChange={this.update.bind(this)}/>
+
+                <h1> {this.state.txt} </h1>
             </div>
         );
     }
 }
 
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-    txt: 'this is my first component'
-}
-
-ReactDOM.render(
-    <App cat={5} txt="this is the props text" />,
-    // <App cat={5} />, then will show the default props.txt
-    document.getElementById('app')
-);
+export default App
